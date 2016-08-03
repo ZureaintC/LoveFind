3a1cbce8753491b0e80de3e6db30b70d5e39de57
付晨 <fuchen@bogon>
2016-08-03 14:09:57 +0800

    Initial Commit

---
+++ LoveFind/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LoveFind
+//
+//  Created by 付晨 on 16/8/3.
+//  Copyright © 2016年 付晨. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
